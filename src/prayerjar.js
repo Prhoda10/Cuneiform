5112c1abd733aa95881aed00a99d83a27563374d
Gloria Kim <gloria.kim@my.wheaton.edu>
2022-04-09 11:53:45 -0500

    merge conflict

--- src/prayerjar.js
+++ src/prayerjar.js
@@ -1,32 +1,32 @@
-import { getFirestore, addDoc, collection, serverTimestamp} from "firebase/firestore";
+import { getFirestore, addDoc, collection, serverTimestamp } from "firebase/firestore";
 // Saves a new message to Cloud Firestore.
 async function saveMessage(msg) {
 	// Add a new message entry to the Firebase database.
 	try {
-	 	const docRef = await addDoc(collection(getFirestore(), 'prayer'), {
-		text: msg,
-		timestamp: serverTimestamp()
-	  });
-	  console.log("Prayer submitted: ", docRef.id);
+		const docRef = await addDoc(collection(getFirestore(), 'prayer'), {
+			text: msg,
+			timestamp: serverTimestamp()
+		});
+		console.log("Prayer submitted: ", docRef.id);
 	}
-	catch(error) {
-	  console.error('Error writing new message to Firebase Database', error);
+	catch (error) {
+		console.error('Error writing new message to Firebase Database', error);
 	}
-  }
+}
 
-  import { query, getDocs } from "firebase/firestore";
+import { query, getDocs } from "firebase/firestore";
 
-  async function readMessage() {
-  const q = query(collection(getFirestore(), 'prayer'));
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-	console.log(doc.id, " => ", doc.data());
-	let li = document.createElement("li");
-	li.setAttribute('id', prayer);
-	li.appendChild(document.createTextNode(doc.data().text));
-	document.getElementById("prayerList").appendChild(li)
-  })
-  }
+async function readMessage() {
+	const q = query(collection(getFirestore(), 'prayer'));
+	const querySnapshot = await getDocs(q);
+	querySnapshot.forEach((doc) => {
+		console.log(doc.id, " => ", doc.data());
+		let li = document.createElement("li");
+		li.setAttribute('id', prayer);
+		li.appendChild(document.createTextNode(doc.data().text));
+		document.getElementById("prayerList").appendChild(li)
+	})
+}
 
 /*
 * connect functions to buttons for this specific page
@@ -61,7 +61,7 @@ function submitPrayer() {
 function removePrayer() {
 	let list = document.getElementById("prayerList");
 	while (list.firstChild) {
-	  list.removeChild(addListener.lastChild);
+		list.removeChild(addListener.lastChild);
 	}
 	readMessage();
 }
@@ -74,7 +74,7 @@ function removePrayer() {
 function checkPrayer(x, words) {
 	for (let i = 0; i < words.length; i++) {
 		if (x.includes(words[i])) {
-			document.getElementById("invalidPrayerInput").innerHTML = "Prayer contains unaccepable language";
+			document.getElementById("invalidPrayerInput").innerHTML = "Prayer contains unacceptable language";
 			return true;
 		}
 	}
