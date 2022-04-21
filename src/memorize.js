005421896ff2c3c1e1f5c09fe2a9d03bcd27f336
Zwacky7 <ecliff09@gmail.com>
2022-04-20 20:24:07 -0500

    Decks must now have a name

--- src/memorize.js
+++ src/memorize.js
@@ -86,6 +86,10 @@ async function exportFlashcards() {
 		document.getElementById("deck-name").value = "Deck name must be under 20 characters!";
 		return;
 	}
+	else if(deckName == "") {
+		document.getElementById("deck-name").value = "Deck must be named!";
+		return;
+	}
 	try {
 		const docRef = await addDoc(collection(db, "flashcardDecks"), {
 			name: deckName,
